f555c3059647c45c1c831ef03208b4a564acce25
Muhammad Amien Rauf <muhammadamien23@gmail.com>
2022-10-22 22:30:45 +0700

    feat(main):Slicing page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   env: {
+//     AIRTABLE_API_KEY: "keyHA9y1ULTuiFbip",
+//     AIRTABLE_BASE: "apprPGufGULFFjdvQ",
+//     AIRTABLE_TABLE: "chair",
+//   }
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  reactStrictMode: true,
+  env: {
+    AIRTABLE_API_KEY: "keyHA9y1ULTuiFbip",
+    AIRTABLE_BASE: "apprPGufGULFFjdvQ",
+    AIRTABLE_TABLE: "chair",
+  },
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import '@popperjs/core'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
