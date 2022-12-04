407e799279b0194e249c60dec5c4674b7a4802d7
Muhammad Amien Rauf <muhammadamien23@gmail.com>
2022-12-04 13:28:13 +0700

    initialize rewrite app

--- pages/api/utils/Airtable.js
+++
@@ -1,9 +0,0 @@
-import Airtable from 'airtable';
-
-Airtable.configure({
-    apiKey: process.env.AIRTABLE_API_KEY,
-});
-
-const base = Airtable.base(process.env.AIRTABLE_BASE);
-
-export { base };
